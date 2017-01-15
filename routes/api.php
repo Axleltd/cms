0b08d0b102477ba730be69dacbdc813deba6932c
Ramesh Paudel <ramesh@axle.com.np>
2017-01-15 13:32:21 +0545

    Added Api Routes

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,35 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
 
+//Users
 Route::get('/users','UserController@index');
+
+//Pages
 Route::get('/pages','PageController@index');
 Route::get('/pages/{id}','PageController@show');
+
+//Post
 Route::get('/posts','PostController@index');
 Route::get('/posts/{id}','PostController@show');
+
+//Calendar
+Route::get('/calendars','CalendarController@index');
+Route::get('/calendars/{id}','CalendarController@show');
+//Resource
+Route::get('/resources','ResourceController@index');
+Route::get('/resources/{id}','ResourceController@show');
+//Staff
+
+Route::get('/staffs','StaffController@index');
+Route::get('/staffs/{id}','StaffController@show');
+//Student
+
+Route::get('/students','StudentController@index');
+Route::get('/students/{id}','StudentController@show');
+//Gallery
+
+Route::get('/galleries','GalleryController@index');
+Route::get('/galleries/{id}','GalleryController@show');
+//News
+Route::get('/news','NewsController@index');
+Route::get('/news/{id}','NewsController@show');
